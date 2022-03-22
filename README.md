7040921da361e9dc5002779db27547a6673dc122
Irakli Mchedlishvili <ir.mchedlishvili@gmail.com>
2022-03-22 18:27:33 +0400

    [all][s]: Initial script for data extraction + README

---
+++ README.md
@@ -0,0 +1,25 @@
+Scripts for extracting, transforming and loading data from Fullnode on Binance Smart Chain.
+This should in theory work with any other network, but is tested on BSC only.
+
+## Prerequisites
+
+* Make sure you have access Binance Smart Chain Fullnode
+  * To run your own node follow instuctions https://docs.binance.org/smart-chain/developer/fullnode.html
+  * Alternatevaly you can use QuickNode, Moralis or any of your choice
+* Install Ethereum ETL `pip install ethereum-etl`
+
+## Configure
+
+Node Provider is by default set to `file:///home/ubuntu/node/geth.ipc`,
+assuming you are running BSC node of your own. You can change that by setting
+`PROVIDER_URL` Eg:
+
+```
+PROVIDER_URL=https://speedy-nodes-nyc.moralis.io/1234567890/bsc/mainnet
+```
+
+## Run Scripts
+
+```
+python extract.py
+```
---
+++ extract.py
@@ -0,0 +1,62 @@
+import datetime
+import os
+
+from blockchainetl.logging_utils import logging_basic_config
+from ethereumetl.service.eth_service import EthService, BlockTimestampGraph
+from ethereumetl.providers.auto import get_provider_from_uri
+from ethereumetl.utils import check_classic_provider_uri
+from ethereumetl.web3_utils import build_web3
+from ethereumetl.jobs.export_blocks_job import ExportBlocksJob
+from ethereumetl.jobs.exporters.blocks_and_transactions_item_exporter import blocks_and_transactions_item_exporter
+from ethereumetl.thread_local_proxy import ThreadLocalProxy
+
+logging_basic_config()
+
+this_hour = datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0, second=0, minute=0)
+hour_ago = this_hour - datetime.timedelta(hours=1)
+this_hour_ts = this_hour.timestamp()
+hour_ago_ts = hour_ago.timestamp()
+
+provider_uri = os.environ.get('PROVIDER_URL', 'file:///home/ubuntu/node/geth.ipc')
+
+def get_block_range_for_timestamps(provider_uri, start_timestamp, end_timestamp, chain='binance'):
+    """Outputs start and end blocks for given timestamps."""
+    provider_uri = check_classic_provider_uri(chain, provider_uri)
+    provider = get_provider_from_uri(provider_uri)
+    web3 = build_web3(provider)
+    eth_service = EthService(web3)
+
+    return eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
+
+
+def export_blocks_and_transactions(start_block, end_block, batch_size, provider_uri, max_workers, blocks_output,
+                                   transactions_output, chain='binance'):
+    """Exports blocks and transactions."""
+    provider_uri = check_classic_provider_uri(chain, provider_uri)
+    if blocks_output is None and transactions_output is None:
+        raise ValueError('Either --blocks-output or --transactions-output options must be provided')
+
+    job = ExportBlocksJob(
+        start_block=start_block,
+        end_block=end_block,
+        batch_size=batch_size,
+        batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
+        max_workers=max_workers,
+        item_exporter=blocks_and_transactions_item_exporter(blocks_output, transactions_output),
+        export_blocks=blocks_output is not None,
+        export_transactions=transactions_output is not None)
+    job.run()
+
+if __name__ == '__main__':
+    start_block, end_block = get_block_range_for_timestamps(provider_uri, hour_ago_ts, this_hour_ts)
+    _date, this_hour = str(this_hour).replace('+00:00', '').split(' ')
+    _date, hour_ago = str(hour_ago).replace('+00:00', '').split(' ')
+    export_blocks_and_transactions(
+        start_block=start_block,
+        end_block=end_block,
+        batch_size=end_block - start_block,
+        provider_uri=provider_uri,
+        max_workers=5,
+        blocks_output='/tmp/dummy.csv',
+        transactions_output=f'~/data/extracted/{_date}/{hour_ago}-{this_hour}.csv'
+    )
