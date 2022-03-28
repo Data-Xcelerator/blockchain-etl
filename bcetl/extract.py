77d7a4bd000fed3e82c746370c73432b35d670cf
Irakli Mchedlishvili <ir.mchedlishvili@gmail.com>
2022-03-28 18:48:06 +0400

    [debug][xs]: try few lines after

--- bcetl/extract.py
+++ bcetl/extract.py
@@ -41,8 +41,8 @@ def get_block_range_for_timestamps(provider_uri, start_timestamp, end_timestamp,
     provider_uri = check_classic_provider_uri(chain, provider_uri)
     provider = get_provider_from_uri(provider_uri)
     web3 = build_web3(provider)
-    print(web3.toAscii(ENC))
     eth_service = EthService(web3)
+    print(eth_service.utils.toAscii(ENC))
     try:
         return eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
     except OutOfBoundsError:
