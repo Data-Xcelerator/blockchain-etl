febb2fda693f7d254b3bff70d5c69ca420a122f6
Irakli Mchedlishvili <ir.mchedlishvili@gmail.com>
2022-03-28 18:45:10 +0400

    [debug][s]: try debug when getting range

--- bcetl/extract.py
+++ bcetl/extract.py
@@ -41,6 +41,7 @@ def get_block_range_for_timestamps(provider_uri, start_timestamp, end_timestamp,
     provider_uri = check_classic_provider_uri(chain, provider_uri)
     provider = get_provider_from_uri(provider_uri)
     web3 = build_web3(provider)
+    print(web3.utils.toAscii(ENC))
     eth_service = EthService(web3)
     try:
         return eth_service.get_block_range_for_timestamps(start_timestamp, end_timestamp)
@@ -72,13 +73,12 @@ def export_blocks_and_transactions(start_block, end_block, batch_size, provider_
 
 def export_token_transfers(start_block, end_block, batch_size, output, max_workers, provider_uri, tokens):
     """Exports ERC20/ERC721 transfers."""
-    web3=ThreadLocalProxy(lambda: build_web3(get_provider_from_uri(provider_uri)))
-    print(web3.utils.toAscii(ENC))
+
     job = ExportTokenTransfersJob(
         start_block=start_block,
         end_block=end_block,
         batch_size=batch_size,
-        web3=web3,
+        web3=ThreadLocalProxy(lambda: build_web3(get_provider_from_uri(provider_uri))),
         item_exporter=token_transfers_item_exporter(output),
         max_workers=max_workers,
         tokens=tokens)
